bfc68922ed9b4965d9e288fa0be51c79e664ae51
Adam Kruschwitz <akruschwitz@ithaca.edu>
2021-05-09 13:18:26 -0700

    Reminder table + shell script

--- app/models.py
+++ app/models.py
@@ -1,5 +1,5 @@
-from app import db
-from app import login
+from app import db, login
+from datetime import datetime
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask_login import UserMixin
 
@@ -8,6 +8,7 @@ class User(UserMixin, db.Model):
     username = db.Column(db.String(64))
     phone_number = db.Column(db.String(10), index=True, unique=True)
     password_hash = db.Column(db.String(128))
+    reminders = db.relationship('Reminder', backref='owner', lazy='dynamic')
 
     def set_password(self, password):
         self.password_hash = generate_password_hash(password)
@@ -16,7 +17,17 @@ class User(UserMixin, db.Model):
         return check_password_hash(self.password_hash, password)
 
     def __repr__(self):
-        return '<User {}>'.format(self.firstName)
+        return '<User {}>'.format(self.username)
+
+class Reminder(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    text = db.Column(db.String(160))
+    date_to_send = db.Column(db.DateTime, default=datetime.utcnow)
+    date_last_sent = db.Column(db.DateTime)
+    user_id = db.column(db.Integer, db.ForeignKey('user.id'))
+
+    def __repr__(self):
+        return '<Reminder {}>'.format(self.text)
 
 @login.user_loader
 def load_user(id):
